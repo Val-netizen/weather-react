6e9c2c6db27cfb4625ace4ca1d9da9d9348d055b
Val-netizen <cacciapaglia.valeria@gmail.com>
2021-02-06 14:25:09 +0000

    added <div> to footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Footer(){
     return (
+        <div>
         <small>
             Open-source code by Valeria Cacciapaglia
         </small>
+        </div>
     )
 }
