68836d85ebd6350d03be1a48f843f23cf63f7f81
Val-netizen <cacciapaglia.valeria@gmail.com>
2021-02-06 15:13:22 +0000

    removed blank from link

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
     return (
         <div>
         <small>
-            <a href="https://github.com/Val-netizen/weather-react" target="_blank">Open-source code</a> by Valeria Cacciapaglia
+            <a href="https://github.com/Val-netizen/weather-react">Open-source code</a> by Valeria Cacciapaglia
         </small>
         </div>
     )
