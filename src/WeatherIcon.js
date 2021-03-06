3acf7adcac04bf052e387a412e1de377918f82aa
Val-netizen <cacciapaglia.valeria@gmail.com>
2021-03-06 18:43:08 +0000

    changed color of icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color= "#da4453"
+      color= "#24675F"
       size={52}
       animate={true}
     />
