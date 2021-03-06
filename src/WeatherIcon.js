585feaab49d226fbe3aca097c535f6cbc98c7aca
Val-netizen <cacciapaglia.valeria@gmail.com>
2021-03-06 18:46:57 +0000

    icon color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color= "#24675F"
+      color= "#FFFFFF"
       size={52}
       animate={true}
     />
