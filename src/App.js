149e7df723cd3c217f48b3050f102a6d917c3dbd
Val-netizen <cacciapaglia.valeria@gmail.com>
2021-03-07 13:05:28 +0000

    changed default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Dublin"/>
+        <Weather defaultCity="Dublino"/>
         <Footer />
       </div>
     </div>
